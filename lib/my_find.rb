dd57ffd0e3486be8c4c765d41c812900afb09e3d
LaNice Powell <lanicemp@aol.com>
2019-07-24 15:00:49 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,10 +3,9 @@ require 'pry'
 def my_find(collection)
   i = 0 
   while i < collection.length 
-   
-     return collection[i]
+     return collection[i] if yield(collection[i])
     i = i + 1 
-    binding.pry 
+    #binding.pry 
   end 
 end
 
